d36858c3cb3dc843a733442b762536c8d0a2891f
Milena Okumoto <milena.okumoto@gmail.com>
2021-03-15 20:36:40 -0300

    Teste

---
+++ src/test/java/steps/Post.java
@@ -0,0 +1,34 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import static org.junit.Assert.assertTrue;
+
+public class Post extends Base {
+    private Base base;
+
+    public Post(Base base) { //construtor
+        this.base = base;
+    }
+
+    @Dado("^que acesso a Wikipedia em Portugues$")
+    public void queAcessoAWikipediaEmPortugues() {
+        base.driver.get(base.url);
+        //Abre o navegador no site alvo(extendendo da base)
+    }
+
+    @Quando("^pesquiso por \"([^\"]*)\"$")
+    public void pesquisoPor(String produto) {
+        base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);
+    }
+
+    @Entao("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
+    public void exibeAExpressaoNoTituloDaGuia(String produto) {
+        assertTrue(base.driver.getTitle().contains(produto));
+    }
+}
