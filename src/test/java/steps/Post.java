fb74ee874c18b7ca35cdee0ce0fd5b75e10b9724
Milena Okumoto <milena.okumoto@gmail.com>
2021-03-15 23:19:21 -0300

    Teste 2

--- src/test/java/steps/Post.java
+++ src/test/java/steps/Post.java
@@ -1,9 +1,9 @@
 package steps;
 
 import cucumber.api.PendingException;
-import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Entao;
-import cucumber.api.java.pt.Quando;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
@@ -15,7 +15,7 @@ public class Post extends Base {
     public Post(Base base) { //construtor
         this.base = base;
     }
-
+/*
     @Dado("^que acesso a Wikipedia em Portugues$")
     public void queAcessoAWikipediaEmPortugues() {
         base.driver.get(base.url);
@@ -28,6 +28,26 @@ public class Post extends Base {
     }
 
     @Entao("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
+    public void exibeAExpressaoNoTituloDaGuia(String produto) {
+        assertTrue(base.driver.getTitle().contains(produto));
+    }
+}
+*/
+
+//
+
+    @Given("^que acesso a Wikipedia em Portugues$")
+    public void queAcessoAWikipediaEmPortugues() {
+        base.driver.get(base.url);
+        //Abre o navegador no site alvo(extendendo da base)
+    }
+
+    @When("^pesquiso por \"([^\"]*)\"$")
+    public void pesquisoPor(String produto) {
+        base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);
+    }
+
+    @Then("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
     public void exibeAExpressaoNoTituloDaGuia(String produto) {
         assertTrue(base.driver.getTitle().contains(produto));
     }
